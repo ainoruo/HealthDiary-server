9dee35723e0934394b301028be1b5992110d03b0
ainoruo <aino.ruohola@metropolia.fi>
2024-01-22 08:40:00 +0200

    muokattu

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,10 @@
 module.exports = {
   'env': {
-    'browser': true,
     'es2021': true,
+    'node': true,
   },
   'extends': 'google',
   'overrides': [
-    {
-      'env': {
-        'node': true,
-      },
-      'files': [
-        '.eslintrc.{js,cjs}',
-      ],
-      'parserOptions': {
-        'sourceType': 'script',
-      },
-    },
   ],
   'parserOptions': {
     'ecmaVersion': 'latest',
