d4e5628644d20f3b185ae006df17d3aed4646d38
ainoruo <aino.ruohola@metropolia.fi>
2024-03-14 11:06:55 +0200

    added edits

---
+++ public/assets/info-DRR4x_Ec.js
@@ -0,0 +1,1 @@
+import"./style-6mOx-azO.js";document.addEventListener("DOMContentLoaded",function(){const e=document.querySelector(".menu-toggle"),n=document.querySelector(".menu");e.addEventListener("click",function(){n.classList.toggle("show")})});
