4ff2f14ca91b416f5a2fd5078a7a3807e668bc82
ainoruo <aino.ruohola@metropolia.fi>
2024-03-15 10:41:25 +0200

    added edits

---
+++ public/assets/info-B-t5v6JU.js
@@ -0,0 +1,1 @@
+import"./style-rr3s28mv.js";document.addEventListener("DOMContentLoaded",function(){const e=document.querySelector(".menu-toggle"),n=document.querySelector(".menu");e.addEventListener("click",function(){n.classList.toggle("show")})});
