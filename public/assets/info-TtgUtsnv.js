ed9dcdd9d7d5a898d4c87fb29b91a0faaee4f8ef
ainoruo <aino.ruohola@metropolia.fi>
2024-03-15 09:13:34 +0200

    added edits

---
+++ public/assets/info-TtgUtsnv.js
@@ -0,0 +1,1 @@
+import"./style-CNpknRdY.js";document.addEventListener("DOMContentLoaded",function(){const e=document.querySelector(".menu-toggle"),n=document.querySelector(".menu");e.addEventListener("click",function(){n.classList.toggle("show")})});
