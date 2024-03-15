78e3870c3032c0806051daa414e29f2334af6e83
ainoruo <aino.ruohola@metropolia.fi>
2024-03-15 10:29:39 +0200

    added edits

---
+++ public/assets/info-C7WLE1Hq.js
@@ -0,0 +1,1 @@
+import"./style-NmStaRgG.js";document.addEventListener("DOMContentLoaded",function(){const e=document.querySelector(".menu-toggle"),n=document.querySelector(".menu");e.addEventListener("click",function(){n.classList.toggle("show")})});
